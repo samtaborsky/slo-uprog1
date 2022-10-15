5a177e6a5e261b6b03a5dc78c07628568b67adbe
Samuel Táborský <s.taborsky1@gmail.com>
2022-10-15 18:58:34 +0200

    U0,1 fix

--- l03/l03_u0_u1.py
+++ l03/l03_u0_u1.py
@@ -1,79 +1,79 @@
 def vytvor(n, x):
-    m = [[x for a in range(n)] for b in range(n)]
+    m = [[x for _ in range(n)] for _ in range(n)]
     return m
 
 
-def tisk(A):
-    for x in A:
+def tisk(matice):
+    for x in matice:
         for y in x:
             print('{0:5} '.format(y), end='')
         print()
 
 
-def soucin(A, B):
-    global C
-    sloupce_a = len(A[0])
-    sloupce_b = len(B[0])
-    radky_a = len(A)
-    radky_b = len(B)
+def soucin(matice_a, matice_b):
+    sloupce_a = len(matice_a[0])
+    sloupce_b = len(matice_b[0])
+    radky_a = len(matice_a)
+    radky_b = len(matice_b)
     if sloupce_a != radky_b:
         exit('Spatne matice.')
     else:
-        C = [[0 for x in range(sloupce_b)] for y in range(radky_a)]
+        matice_c = [[0 for _ in range(sloupce_b)] for _ in range(radky_a)]
         for r in range(radky_a):
             for s in range(sloupce_b):
                 for i in range(radky_b):
-                    C[r][s] += A[r][i] * B[i][s]
-        return C
+                    matice_c[r][s] += matice_a[r][i] * matice_b[i][s]
+        return matice_c
 
 
-def nastav_okoli(A, i, j, c):
+def nastav_okoli(matice_a, i, j, c):
     """Funkce bere bezne indexy matic od 1 po N"""
-    n = len(A) - 1
+    n = len(matice_a) - 1
     i -= 1
     j -= 1
     if j < 0 or i < 0 or i > n or j > n:
         exit('Spatne zadane indexy!')
-    A[i][j] = c
+    matice_a[i][j] = c
     if i == n:
         if j == 0:
-            A[i][j+1] = c
-            A[i-1][j] = c
+            matice_a[i][j+1] = c
+            matice_a[i-1][j] = c
         elif j == n:
-            A[i][j-1] = c
-            A[i-1][j] = c
+            matice_a[i][j-1] = c
+            matice_a[i-1][j] = c
         else:
-            A[i][j-1] = c
-            A[i][j+1] = c
-            A[i-1][j] = c
+            matice_a[i][j-1] = c
+            matice_a[i][j+1] = c
+            matice_a[i-1][j] = c
 
     elif i == 0:
         if j == 0:
-            A[i][j+1] = c
-            A[i+1][j] = c
+            matice_a[i][j+1] = c
+            matice_a[i+1][j] = c
         elif j == n:
-            A[i][j-1] = c
-            A[i+1][j] = c
+            matice_a[i][j-1] = c
+            matice_a[i+1][j] = c
         else:
-            A[i][j-1] = c
-            A[i][j+1] = c
-            A[i+1][j] = c
+            matice_a[i][j-1] = c
+            matice_a[i][j+1] = c
+            matice_a[i+1][j] = c
 
     else:
         if j == 0:
-            A[i][j+1] = c
-            A[i-1][j] = c
-            A[i+1][j] = c
+            matice_a[i][j+1] = c
+            matice_a[i-1][j] = c
+            matice_a[i+1][j] = c
         elif j == n:
-            A[i][j-1] = c
-            A[i-1][j] = c
-            A[i+1][j] = c
+            matice_a[i][j-1] = c
+            matice_a[i-1][j] = c
+            matice_a[i+1][j] = c
         else:
-            A[i][j-1] = c
-            A[i][j+1] = c
-            A[i-1][j] = c
-            A[i+1][j] = c
-    return A
+            matice_a[i][j-1] = c
+            matice_a[i][j+1] = c
+            matice_a[i-1][j] = c
+            matice_a[i+1][j] = c
+
+    return matice_a
 
 
 A = vytvor(5, 0)
