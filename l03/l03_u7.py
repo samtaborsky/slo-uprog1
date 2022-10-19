3291cf1b67592bc91474f4fd1c82ec10914afa56
Samuel Táborský <s.taborsky1@gmail.com>
2022-10-19 23:44:38 +0200

    Range edit

--- l03/l03_u7.py
+++ l03/l03_u7.py
@@ -6,7 +6,7 @@ slova = []
 slovo = ''
 
 for y in range(1, random.randint(1, 10)):
-    for x in range(3, random.randint(4, 10)):
+    for x in range(3, random.randint(3, 10)):
         slovo += random.choice(souhlasky)
         slovo += random.choice(samohlasky)
     slova.append(slovo)
