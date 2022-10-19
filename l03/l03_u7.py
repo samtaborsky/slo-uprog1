cb50fa872f0bbcaef76717fb2ea1cf335af9be4d
Samuel Táborský <s.taborsky1@gmail.com>
2022-10-19 23:00:00 +0200

    U7 done

---
+++ l03/l03_u7.py
@@ -0,0 +1,18 @@
+import random
+
+souhlasky = ['h', 'ch', 'k', 'r', 'd', 't', 'n', 'b', 'f', 'l', 'm', 'p', 's', 'v', 'z', 'c', 'j']
+samohlasky = ['a', 'e', 'i', 'o', 'u']
+slova = []
+slovo = ''
+
+for y in range(1, random.randint(1, 10)):
+    for x in range(3, random.randint(4, 10)):
+        slovo += random.choice(souhlasky)
+        slovo += random.choice(samohlasky)
+    slova.append(slovo)
+    slovo = ''
+
+n = 0
+for x in slova:
+    print(f'{n} {x}')
+    n += 1
